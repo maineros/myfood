d477590f6acd5620a7682ff399be7eeeb0893e1f
mainero <mainerolaura@gmail.com>
2026-05-07 21:04:15 -0300

    add: atributo entregadores para manter os entregadores cadastrados

--- src/models/Empresa.java
+++ src/models/Empresa.java
@@ -1,35 +1,32 @@
 package models;
 
-public class Empresa {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Empresa {
     private int id;
     private int idDono;
     private String nome;
     private String endereco;
-    private String tipoCozinha;
-
-    // xmldecoder
+    private List<Integer> entregadores = new ArrayList<>();
+    
     public Empresa() {}
 
-    public Empresa(int id, int idDono, String nome, String endereco, String tipoCozinha) {
+    public Empresa(int id, int idDono, String nome, String endereco) {
         this.id = id;
         this.idDono = idDono;
         this.nome = nome;
         this.endereco = endereco;
-        this.tipoCozinha = tipoCozinha;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
     public int getIdDono() { return idDono; }
     public void setIdDono(int idDono) { this.idDono = idDono; }
-
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-
     public String getEndereco() { return endereco; }
     public void setEndereco(String endereco) { this.endereco = endereco; }
-
-    public String getTipoCozinha() { return tipoCozinha; }
-    public void setTipoCozinha(String tipoCozinha) { this.tipoCozinha = tipoCozinha; }
+    public List<Integer> getEntregadores() { return entregadores; }
+    public void setEntregadores(List<Integer> entregadores) { this.entregadores = entregadores; }
 }
