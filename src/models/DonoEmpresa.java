3c37c55480a1887d0ee5ea975040803f753a899d
Laura Mainero <mainerolaura@gmail.com>
2026-05-17 17:02:46 -0300

    fix: sobrescreve getAtributoEspecifico e isDonoEmpresa

--- src/models/DonoEmpresa.java
+++ src/models/DonoEmpresa.java
@@ -1,5 +1,7 @@
 package models;
 
+import exceptions.usuario.UsuarioException;
+
 public class DonoEmpresa extends Usuario {
     private String cpf;
 
@@ -10,7 +12,25 @@ public class DonoEmpresa extends Usuario {
         super(id, nome, email, senha, endereco);
         this.cpf = cpf;
     }
-    
+
+    /**
+     * Retorna o valor de atributos específicos do DonoEmpresa.
+     *
+     * @param atributo nome do atributo ("cpf")
+     * @return valor do atributo como String
+     * @throws UsuarioException se o atributo não existir para DonoEmpresa
+     */
+    @Override
+    public String getAtributoEspecifico(String atributo) throws UsuarioException {
+        if ("cpf".equals(atributo)) return this.cpf;
+        throw new UsuarioException("Usuario nao possui este atributo");
+    }
+
+    @Override
+    public boolean isDonoEmpresa() {
+        return true;
+    }
+
     public String getCpf() { return cpf; }
     public void setCpf(String cpf) { this.cpf = cpf; }
 }
