1e9753ff2ca693ce2b29172060ed1ce82b0fdbf3
Laura Mainero <mainerolaura@gmail.com>
2026-05-17 17:10:36 -0300

    add: adiciona exceção para horário de funcionamento inválido

---
+++ src/exceptions/empresa/HorarioInvalidoException.java
@@ -0,0 +1,7 @@
+package exceptions.empresa;
+
+public class HorarioInvalidoException extends EmpresaException {
+    public HorarioInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
