a9ac4d85b6471f2f8a1b42ef851affe1e8b7116f
Laura Mainero <mainerolaura@gmail.com>
2026-05-17 17:11:24 -0300

    add: adiciona exceção para empresa duplicada

---
+++ src/exceptions/empresa/EmpresaDuplicadaException.java
@@ -0,0 +1,7 @@
+package exceptions.empresa;
+
+public class EmpresaDuplicadaException extends EmpresaException {
+    public EmpresaDuplicadaException(String mensagem) {
+        super(mensagem);
+    }
+}
