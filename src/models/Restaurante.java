a1f1538840dd702d37fa7996080e36955160bf85
Laura Mainero <mainerolaura@gmail.com>
2026-05-17 17:01:24 -0300

    fix: sobrescreve getAtributoEspecifico

--- src/models/Restaurante.java
+++ src/models/Restaurante.java
@@ -1,5 +1,7 @@
 package models;
 
+import exceptions.empresa.EmpresaException;
+
 public class Restaurante extends Empresa {
     private String tipoCozinha;
 
@@ -8,6 +10,19 @@ public class Restaurante extends Empresa {
     public Restaurante(int id, int idDono, String nome, String endereco, String tipoCozinha) {
         super(id, idDono, nome, endereco);
         this.tipoCozinha = tipoCozinha;
+    }
+
+    /**
+     * Retorna o valor de atributos específicos do Restaurante.
+     *
+     * @param atributo nome do atributo ("tipoCozinha")
+     * @return valor do atributo como String
+     * @throws EmpresaException se o atributo não existir para Restaurante
+     */
+    @Override
+    public String getAtributoEspecifico(String atributo) throws EmpresaException {
+        if ("tipoCozinha".equals(atributo)) return this.tipoCozinha;
+        throw new EmpresaException("Atributo invalido");
     }
 
     public String getTipoCozinha() { return tipoCozinha; }
